d389de88dd7333bdf96a6dccbd72e50fa10752d8
dinaalamin00 <115608805+dinaalamin00@users.noreply.github.com>
2025-08-29 16:46:37 +0000

    added network to docker compose services, fixed env file and calls in the scripts db and wd, queres

--- srcs/requirements/mariadb/scripts.sh
+++ srcs/requirements/mariadb/scripts.sh
@@ -1,64 +1,101 @@
-#!/bin/bash
-set -e
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing MariaDB data directory..."
-    mysql --initialize-insecure --user=mysql
+# #!/bin/bash
+# set -e
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
+#     echo "Initializing MariaDB data directory..."
+#     mysql --initialize-insecure --user=mysql
+# fi
+# # Start MariaDB in background for setup
+# mysqld_safe --skip-networking &
+# # Wait until MariaDB is ready
+# until mysqladmin ping >/dev/null 2>&1; do
+#     sleep 2
+# done
+
+# if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+#     echo "Setting uo root user..."
+#     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# fi
+# # Optional database
+# if [ -n "$MYSQL_DATABASE" ]; then
+#     echo "Creating database $MYSQL_DATABASE..."
+#     mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+# fi
+
+# # Optional non-root user
+# if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
+#     echo "Creating user $MYSQL_USER..."
+#     mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+#     mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
+# fi
+
+# for f in /docker-entrypoint-initdb.d/*; do
+#     case "$f" in
+#         *.sh)  echo "Running $f"; . "$f" ;;
+#         *.sql) echo "Running $f"; mysql < "$f" ;;
+#         *)     echo "Ignoring $f" ;;
+#     esac
+# done
+
+
+# exec mysqld
+
+# # cmd - 1
+#     # ensure the scirpt runs in bash
+#     # the first 2 characters is called shebang,
+#     # shebang is the first line in a script filethat tells the operating system which interpreter to use to run the script
+# # cmd - 2
+#     # add safety options to stop the script immediately if anycommand fails, preventing silent errors
+# # cmd - 3
+#     # initialize the database if empty
+#     # 1 checks if the database directory is empty ( -d<paht> returns true if the directory exists)
+#     # 3 tells mariadb to initialize the data directory without setting a root password
+#     # why "insecure"? the root has not password after initialization,
+#         # the root password is set later in the script 
+#     # --initialize (wihtou insecure) generates a random root password
+#     # --user=mysql insures the mariadb runs the initialization process as the mysql system user, not root
+#         #important for correct file permissions in /var/lib/mysql so the server can read/write data properly
+# # cmd - 4
+#     # create root user and optional database/user
+#     # -n tests id the dtring is not empty
+#     # mysql runs the MySQL command-line client
+#     # -e allows executing a single SQL command directly without opening an interactive shell
+#     # "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" change the password of the root to the one in the env
+#     # 'root'@'localhost' refers to the root user connecting from the localhost
+
+#!/usr/bin/env bash
+
+# Allow connections from any IP, not just localhost
+# This is so WP can connect from a separate container
+# Configure MariaDB to listen on all interfaces
+if [ -n "$DB_CONF_ROUTE" ]; then
+  echo >> "$DB_CONF_ROUTE"
+  echo "[mysqld]" >> "$DB_CONF_ROUTE"
+  echo "bind-address=0.0.0.0" >> "$DB_CONF_ROUTE"
 fi
-# Start MariaDB in background for setup
-mysqld_safe --skip-networking &
-# Wait until MariaDB is ready
+# Create database
+mysql_install_db --datadir=$DB_INSTALL
+
+# Start in bg (&)
+mysqld_safe &
+# Store pid for wait cmd
+mysql_pid=$!
+
+# Loop to check if MariaDB has started
+# This prevents WP from connecting before MariaDB is ready
+# Everything still works without this so it
+# might not be necessary. Just a precaution
+# but probably can be removed safely
 until mysqladmin ping >/dev/null 2>&1; do
-    sleep 2
+  echo -n "."; sleep 0.2
 done
 
-if [ -n "$MYSQL_ROOT_PASSWORD" ]; then
-    echo "Setting uo root user..."
-    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-fi
-# Optional database
-if [ -n "$MYSQL_DATABASE" ]; then
-    echo "Creating database $MYSQL_DATABASE..."
-    mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-fi
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
 
-# Optional non-root user
-if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
-    echo "Creating user $MYSQL_USER..."
-    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-    mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-fi
+# Keeps container running until MariaDB process ends
+wait $mysql_pid
 
-for f in /docker-entrypoint-initdb.d/*; do
-    case "$f" in
-        *.sh)  echo "Running $f"; . "$f" ;;
-        *.sql) echo "Running $f"; mysql < "$f" ;;
-        *)     echo "Ignoring $f" ;;
-    esac
-done
-
-# Stop the background server and run the main MariaDB process
-mysqladmin shutdown
-exec mysqld
-
-# cmd - 1
-    # ensure the scirpt runs in bash
-    # the first 2 characters is called shebang,
-    # shebang is the first line in a script filethat tells the operating system which interpreter to use to run the script
-# cmd - 2
-    # add safety options to stop the script immediately if anycommand fails, preventing silent errors
-# cmd - 3
-    # initialize the database if empty
-    # 1 checks if the database directory is empty ( -d<paht> returns true if the directory exists)
-    # 3 tells mariadb to initialize the data directory without setting a root password
-    # why "insecure"? the root has not password after initialization,
-        # the root password is set later in the script 
-    # --initialize (wihtou insecure) generates a random root password
-    # --user=mysql insures the mariadb runs the initialization process as the mysql system user, not root
-        #important for correct file permissions in /var/lib/mysql so the server can read/write data properly
-# cmd - 4
-    # create root user and optional database/user
-    # -n tests id the dtring is not empty
-    # mysql runs the MySQL command-line client
-    # -e allows executing a single SQL command directly without opening an interactive shell
-    # "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" change the password of the root to the one in the env
-    # 'root'@'localhost' refers to the root user connecting from the localhost
--- srcs/requirements/wordpress/scripts.sh
+++ srcs/requirements/wordpress/scripts.sh
@@ -2,10 +2,10 @@
 set -e
 
 #wordpress environmwnt variables
-: "${WP_DB_HOST=mariadb}"
-: "${WP_DB_NAME-wordpress}"
-: "${WP_DB_USER=wp_user}"
-: "${WP_DB_PASSWORD=wp_pass}"
+# : "${WP_DB_HOST=mariadb}"
+# : "${WP_DB_NAME-wordpress}"
+# : "${WP_DB_USER=wp_user}"
+# : "${WP_DB_PASSWORD=wp_pass}"
 
 # Wait for Database Availability until …; do … done
 # WordPress needs the database to exist before starting.
@@ -24,7 +24,7 @@ set -e
 # ✅ This makes the loop quiet, only printing the echo messages you add (like Waiting for database...).
 # Wait 2 seconds before trying again.
 echo "waiting for database to be ready..."
-until mysql -h"$WP_DB_HOST" -u"$WP_DB_USER" -p"$WP_DB_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; do
+until mysql -h"$WORDPRESS_DB_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; do
     sleep 2
 done
 echo "database is ready!"
@@ -33,10 +33,10 @@ echo "database is ready!"
 if [! -f wp-config.php]; then
     echo "creating wp-config.php..."
     cp wp-config-sample.php wp-config.php
-    sed -i "s/database_name_here/$WP_DB_NAME/" wp-config.php
-    sed -i "s/username_here/$WP_DB_USER/" wp-config.php
-    sed -i "s/password_here/$WP_DB_PASSWORD/" wp-config.php
-    sed -i "s/localhost/$WP_DB_HOST/" wp-config.php
+    sed -i "s/database_name_here/$MYSQL_DATABASE/" wp-config.php
+    sed -i "s/username_here/$MYSQL_USER/" wp-config.php
+    sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config.php
+    sed -i "s/localhost/$WORDPRESS_DB_HOST/" wp-config.php
 fi
 
 # Set permissions
@@ -47,10 +47,12 @@ chmod -R 755 /var/www/html
 for f in /docker-entrypoint-initwp.d/*; do
     case "$f" in
         *.sh)  echo "Running $f"; . "$f" ;;
-        *.sql) echo "Running $f"; mysql -h"$WP_DB_HOST" -u"$WP_DB_USER" -p"$WP_DB_PASSWORD" "$WP_DB_NAME" < "$f" ;;
+        *.sql) echo "Running $f"; mysql -h"$WORDPRESS_DB_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < "$f" ;;
         *)     echo "Ignoring $f" ;;
     esac
 done
 
 # Start PHP-FPM
 exec "$@"
+
+ 
