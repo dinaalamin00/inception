f4205e662561e18593e12fda9a1fc82d5c114f6a
dinaalamin00 <115608805+dinaalamin00@users.noreply.github.com>
2025-08-30 09:14:46 +0000

    wordpress & mariadb setup

--- srcs/requirements/wordpress/scripts.sh
+++ srcs/requirements/wordpress/scripts.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 set -e
 
-#wordpress environmwnt variables
-# : "${WP_DB_HOST=mariadb}"
-# : "${WP_DB_NAME-wordpress}"
-# : "${WP_DB_USER=wp_user}"
-# : "${WP_DB_PASSWORD=wp_pass}"
-
-# Wait for Database Availability until …; do … done
-# WordPress needs the database to exist before starting.
-# This loop tries to connect to the DB every 2 seconds until it succeeds.
-# Avoids PHP errors if WordPress starts before MariaDB is ready.
-# This tries to connect to the database using the MySQL client.
-# Flags explained:
-# -h"$WP_DB_HOST" → database host (e.g., mariadb)
-# -u"$WP_DB_USER" → database username
-# -p"$WP_DB_PASSWORD" → password for that user
-# -e "SELECT 1;" → execute a simple SQL command (SELECT 1;) and exit immediately
-# If the connection fails (DB not ready yet), the command returns a non-zero exit code, which keeps the loop running.
-# >/dev/null 2>&1
-# >/dev/null → discard standard output (don’t show it in logs)
-# 2>&1 → redirect standard error to standard output (also discard errors)
-# ✅ This makes the loop quiet, only printing the echo messages you add (like Waiting for database...).
-# Wait 2 seconds before trying again.
 echo "waiting for database to be ready..."
 until mysql -h"$WORDPRESS_DB_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1;" >/dev/null 2>&1; do
     sleep 2
